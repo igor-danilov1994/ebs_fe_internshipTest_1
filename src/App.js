54907c6ec7cb83226f1136c354ab63d57155cecc
Igor <igor.danilov1824@gmail.com>
2021-04-12 16:54:55 +0300

    Test3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const obj = {name: 'Igor',}; obj = "name"
+  const obj={name:'Igor'}
 
   return (
     <div className="App">
